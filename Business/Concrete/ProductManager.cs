be107a9f1988c0d4a4ed2c4a4dcb9696ed4e6d7d
Emine <eminetekin97@outlook.com>
2024-01-15 12:59:15 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using DataAccess.Abstarct;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager:IProductService
+    {
+       
+            IProductDal _IProductDal;
+
+            public ProductManager(IProductDal ProductDal)
+            {
+                _IProductDal = ProductDal;
+            }
+
+            public List<Product> GetAll()
+            {
+                return _IProductDal.GetAll();
+            }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _IProductDal.GetAll(p => p.CategoryId == id);
+         
+        }
+    }
+    }
+
+
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,8 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
