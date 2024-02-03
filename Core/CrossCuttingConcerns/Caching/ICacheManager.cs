fe7d8744042d0001bb504df9ca27da0ff5f48ef0
Emine Tekin <eminetekin97@outlook.com>
2024-02-04 01:36:39 +0300

    Update ICacheManager
    Update ICacheManager

--- Core/CrossCuttingConcerns/Caching/ICacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -14,7 +14,7 @@ namespace Core.CrossCuttingConcerns.Caching
         //cache'de var mı?
         //cacheden silme
         void Remove(string key);
-        //cacheden belirli bir yeri uçurma(",ç,nde get olanlar v.b")
+        //cacheden belirli bir yeri uçurma("içinde get olanlar v.b")
        void RemoveByPattern(string pattern);
         bool IsAdd(string key);
     }
